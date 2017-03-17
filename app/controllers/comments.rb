d163522314e9a1b756ce30728b939f495ddb77d9
Gurkanver Brar & Jordany Rosas <pair+gbrar.jordanyryan@devbootcamp.com>
2017-03-17 14:29:57 -0400

    Worked on fixing comments and added references to user on comment model

--- app/controllers/comments.rb
+++ app/controllers/comments.rb
@@ -9,7 +9,8 @@ post '/questions/:q_id/answers/:a_id' do
   @question = Question.find(params[:q_id])
   @answers = @question.answers
   @answer = Answer.find(params[:a_id])
-  comment = Comment.new(response: params[:comment][:response])
+  @user = User.find(session[:user_id])
+  comment = Comment.new(response: params[:comment][:response], user_id: session[:user_id])
 
   if comment.save
     @answer.comments << comment
