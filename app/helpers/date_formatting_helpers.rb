860e0fc84ddf845d99904665e26c0d72cd2f7134
La-Keisha Towner <ltowner622@gmail.com>
2017-03-19 16:21:44 -0400

    added time formatting to helper

--- app/helpers/date_formatting_helpers.rb
+++ app/helpers/date_formatting_helpers.rb
@@ -2,6 +2,9 @@ module DateFormatting
   def long_form_date(timestamp)
     timestamp.strftime('%B %e, %Y')
   end
+  def time(timestamp)
+    timestamp.strftime("at %I:%M%p")
+  end
 end
 
 helpers DateFormatting
