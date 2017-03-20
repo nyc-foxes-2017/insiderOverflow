8c2e027c581b2a4beb27669ce5ac30279012e42d
Jordany Rosas <email": null>
2017-03-20 08:59:22 -0400

    Made deleting comments functional ajaxed

--- public/js/application.js
+++ public/js/application.js
@@ -88,4 +88,22 @@ $(document).ready(function() {
 
   })
 
+  $(document).on('submit', '.delete-button', function(event){
+    event.preventDefault();
+    var current = $(this)
+    var url = current.attr('action')
+    var method = current.attr('method')
+    var data = current.serialize()
+
+    $.ajax({
+      url: url,
+      method: method,
+      data: data
+    }).done(function(response){
+      current.parent().hide()
+    })
+
+  })
+
+
 });
